5c9f72d1cc4f3adec7483464dad2da2a39f7454a
litieyin <ltieyin@gmail.com>
2021-08-26 15:24:43 +0800

    Update kuwo.js

--- Script/kuwo.js
+++ Script/kuwo.js
@@ -21,13 +21,13 @@ const time = '/vip/spi/mservice';
 
 if (url.indexOf(vip) != -1) {
 	obj.data["isNewUser"] = "2";
-	obj.data["vipLuxuryExpire"] = "1835312949000";
-	obj.data["time"] = "1961170340993";
+	obj.data["vipLuxuryExpire"] = "2524579199000";
+	obj.data["time"] = "2524579199000";
 	obj.data["isYearUser"] = "2";
-	obj.data["vipmExpire"] = "1835312949000";
-	obj.data["vipOverSeasExpire"] = "1835312949000";
-	obj.data["vipExpire"] = "1835312949000";
-	obj.data["vip3Expire"] = "1835312949000";
+	obj.data["vipmExpire"] = "2524579199000";
+	obj.data["vipOverSeasExpire"] = "2524579199000";
+	obj.data["vipExpire"] = "2524579199000";
+	obj.data["vip3Expire"] = "2524579199000";
 	body = JSON.stringify(obj);
 }
 
