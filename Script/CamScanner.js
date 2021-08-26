bdf2357b2a0e77291e40cfe506650ff12eb3e4fb
litieyin <ltieyin@gmail.com>
2021-08-26 10:56:12 +0800

    Create CamScanner.js

---
+++ Script/CamScanner.js
@@ -0,0 +1,16 @@
+/*
+CamScanner 解锁部分高级特权
+
+***************************
+Quantumult X:
+
+[rewrite_local]
+^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
+
+[mitm]
+hostname = ap*.intsig.net
+
+**************************/
+let obj = JSON.parse($response.body);
+obj = {"data":{"psnl_vip_property":{"expiry":"1643731200"}}};
+$done({body: JSON.stringify(obj)});
