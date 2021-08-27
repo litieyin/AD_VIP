e35084a623b33c16b22b726523da76471af906e5
litieyin <ltieyin@gmail.com>
2021-08-27 15:27:12 +0800

    Create jiemian.js

---
+++ Script/jiemian.js
@@ -0,0 +1,16 @@
+  
+/*
+界面新闻解锁会员
+***************************
+QuantumultX:
+[rewrite_local]
+^https:\/\/papi\.jiemian\.com\/app\/user\/* url script-response-body https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/jiemian.js
+^https:\/\/papi\.jiemian\.com\/app\/pro\/* url script-response-body https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/jiemian.js
+[mitm]
+hostname = papi.jiemian.com
+**************************/
+var body = $response.body
+    .replace(/\"is_pro\":0/, "\"is_pro\":1")
+    .replace(/\"is_pay\":1/, "\"is_pay\":0")
+    .replace(/\"free_read_num\":3/, "\"free_read_num\":9999");
+$done({ body });
