356792093eff318d8e7ada9d3b89b85eb068ac25
litieyin <ltieyin@gmail.com>
2021-08-27 22:52:36 +0800

    Update jiemian.js

--- Script/jiemian.js
+++ Script/jiemian.js
@@ -9,9 +9,8 @@ QuantumultX:
 hostname = papi.jiemian.com
 **************************/
 var body = $response.body
-    .replace(/\"is_pro\":0/g, "\"is_pro\":1")
-    .replace(/\"is_pay\":1/g, "\"is_pay\":0")
+    .replace(/\"is_pro\":0/, "\"is_pro\":1")
     .replace(/\"expire_time\":\"\"/, "\"expire_time\":\"2049-12-31\"")
     .replace(/\"free_read_expire\":\"\"/, "\"free_read_expire\":\"2049-12-31\"")
-    .replace(/\"free_read_num\":(\"\d*\"|\d*)/, "\"free_read_num\":99999");
+    .replace(/\"free_read_num\":(\"\d*\"|\d*)/, "\"free_read_num\":4537");
 $done({ body });
