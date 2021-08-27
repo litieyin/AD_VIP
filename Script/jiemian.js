073d7bc14acf78da0dd7d09eedd2a71cbfa5c4af
litieyin <ltieyin@gmail.com>
2021-08-27 23:04:18 +0800

    Update jiemian.js

--- Script/jiemian.js
+++ Script/jiemian.js
@@ -10,7 +10,8 @@ hostname = papi.jiemian.com
 **************************/
 var body = $response.body
     .replace(/\"is_pro\":0/, "\"is_pro\":1")
+    .replace(/\"show_status\":1/, "\"show_status\":1")
     .replace(/\"expire_time\":\"\"/, "\"expire_time\":\"2049-12-31\"")
     .replace(/\"free_read_expire\":\"\"/, "\"free_read_expire\":\"2049-12-31\"")
-    .replace(/\"free_read_num\":(\"\d*\"|\d*)/, "\"free_read_num\":4537");
+    .replace(/\"free_read_num\":(\"\d*\"|\d*)/, "\"free_read_num\":\"4537"\" );
 $done({ body });
