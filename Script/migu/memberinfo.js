1dfeb4cacf5e69d5b2094383ffe8df648dd07e27
litieyin <ltieyin@gmail.com>
2021-08-30 11:03:32 +0800

    Update memberinfo.js

--- Script/migu/memberinfo.js
+++ Script/migu/memberinfo.js
@@ -10,7 +10,7 @@ hostname = v.miguvideo.com
 **************************/
 var body = $response.body
     .replace(/\"2021-09-29 16:50:52\"/p, "\"2049-12-31 23:59:59\"")
-    .replace(/\"isDiamond\":[true|false], "\"isDiamond\":true")
-    .replace(/\"skipBeforeAd\":[true|false], "\"skipBeforeAd\":true")
-    .replace(/\"isVip\":[true|false], "\"isVip\":true")
+    .replace(/\"isDiamond\":[true|false]/, "\"isDiamond\":true")
+    .replace(/\"skipBeforeAd\":[true|false]/, "\"skipBeforeAd\":true")
+    .replace(/\"isVip\":[true|false]/, "\"isVip\":true")
 $done({ body });
