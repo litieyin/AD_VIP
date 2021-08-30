ef03784239b699f2204226439ccf5cd34391a6d7
litieyin <ltieyin@gmail.com>
2021-08-30 10:31:45 +0800

    Create ability.js

---
+++ Script/migu/ability.js
@@ -0,0 +1,16 @@
+/*
+咪咕视频会员到期日
+***************************
+QuantumultX:
+[rewrite_local]
+^https:\/\/papi\.jiemian\.com\/app\/(user|pro|article)\/ url script-response-body https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/jiemian.js
+[mitm]
+hostname = v.miguvideo.com
+**************************/
+var body = $response.body
+    .replace(/\"expiretime\":\"2021-09-29 16:50:52\"/, "\"expiretime\":\"2049-12-31 23:59:59\"")
+    .replace(/\"expiredcardImg\":\"\"/, "\"timesFlag\":\"0\"")
+    .replace(/\"expiredDay\":\"0\"/, "\"sealedFlag\":\"0\"")
+    .replace(/\"effectiveFlag\":\"0\"/, "\"effectiveFlag\":\"1\"")
+    .replace(/\"timeStamp\":\"1630227140760\"/, "\"timeStamp\":2524521600")
+$done({ body });
