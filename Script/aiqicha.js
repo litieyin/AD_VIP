0ad4e55104c36fde8df1b9b6b2afbaed728779a7
litieyin <ltieyin@gmail.com>
2021-08-31 09:52:22 +0800

    Update aiqicha.js

--- Script/aiqicha.js
+++ Script/aiqicha.js
@@ -1,6 +1,6 @@
 /*
 爱企查vip
-https://aiqicha.baidu.com/usercenter/getvipinfoajax
+^https:\//aiqicha.baidu.com\/usercenter\/getvipinfoajax url script-response-body https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/aiqicha.js
 mitm= aiqicha.baidu.com
 */
 
