a196830dfb35d0759537b51804deb29933422f00
litieyin <ltieyin@gmail.com>
2021-08-31 10:05:48 +0800

    Create titan.js

---
+++ Script/titan.js
@@ -0,0 +1,8 @@
+/*
+体坛+vip
+^https:\//api.ttplus.cn\/api\/member_recharge_type\/list url script-response-body https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/titan.js
+mitm= api.ttplus.cn
+*/
+var body = $response.body
+    .replace(/\"expire\":0/, "\"expire\":\"2049-12-31\"");
+$done({ body });
