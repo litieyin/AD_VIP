665f9c90f922334786cdd04adc6136c012f3e336
litieyin <ltieyin@gmail.com>
2021-08-31 10:19:15 +0800

    Delete Script/titan directory

--- Script/titan/1.js
+++
@@ -1,8 +0,0 @@
-/*
-体坛+vip
-^https:\//api.ttplus.cn\/api\/member_recharge_type\/list url script-response-body https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/titan.js
-mitm= api.ttplus.cn
-*/
-var body = $response.body
-    .replace(/\"expire\":0/, "\"expire\":2524521600000");
-$done({ body });
--- Script/titan/2.js
+++
@@ -1,9 +0,0 @@
-/*
-体坛+vip
-^https:\//api.ttplus.cn\/api\/user\/job\/userlist url script-response-body https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/titan/2.js
-mitm= api.ttplus.cn
-*/
-var body = $response.body
-    .replace(/\"is_vip\":0/, "\"is_vip\":1")
-    .replace(/\"vip_expire\":null/, "\"vip_expire\":2524521600000");
-$done({ body });
