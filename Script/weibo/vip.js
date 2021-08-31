ac0c4fb3ca53b675ed35a076d7146465cc9521ed
litieyin <ltieyin@gmail.com>
2021-08-31 14:02:19 +0800

    Update vip.js

--- Script/weibo/vip.js
+++ Script/weibo/vip.js
@@ -10,5 +10,8 @@ hostname = d1025gvspu57dc.cloudfront.net
 var body = $response.body
     .replace(/\"identity\":\"2,0\"/, "\"identity\":\"2,1\"")
     .replace(/\"您还不是微博高级会员\"/, "\"微博高级会员\"")
-    .replace(/\"s_type\":0/, "\"s_type\":1");
+    .replace(/\"s_type\":0/, "\"s_type\":1")
+    .replace(/\"is_baoyue\":0/, "\"is_baoyue\":1")
+    .replace(/\"type\":0/, "\"type\":1")
+    .replace(/\"baoyue_type\":false/, "\"baoyue_type\":true");
 $done({ body });
