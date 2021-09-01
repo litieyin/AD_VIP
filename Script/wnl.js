52f48047dcf349e9c15343196c8445d3366ff9ee
litieyin <ltieyin@gmail.com>
2021-09-01 16:01:22 +0800

    Create wnl.js

---
+++ Script/wnl.js
@@ -0,0 +1,13 @@
+/*
+万年历vip
+***************************
+QuantumultX:
+[rewrite_local]
+^https:\//r.51wnl-cq.com\/Api\/User\/GetExtInfo url script-response-body https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/wnl.js
+[mitm]
+hostname = r.51wnl-cq.com
+**************************/
+var body = $response.body
+    .replace(/\"memberType\":0/, "\"memberType\":1")
+    .replace(/\"memberDate\":\"1970-01-01\"/, "\"memberDate\":\"2049-12-31\"");
+$done({ body });
