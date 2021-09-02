db9a88cf6b4e8e036cc7e8645cccbfadd10613f8
litieyin <ltieyin@gmail.com>
2021-09-02 12:19:37 +0800

    Create people1.js

---
+++ Script/zhihu/people1.js
@@ -0,0 +1,17 @@
+/*
+CamScanner 解锁部分高级特权
+
+***************************
+Quantumult X:
+
+[rewrite_local]
+^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
+
+[mitm]
+hostname = ap*.intsig.net
+
+**************************/
+let obj = JSON.parse($response.body);
+obj = {"data":{"vip_type":"svip"}};
+obj = {"data":{"expire_text" : "读书会员 2049-12-31 到期"}};
+$done({body: JSON.stringify(obj)});
