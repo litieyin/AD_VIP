542b67fd02f960fdfeb2d25aab034cdf2d0e6b42
litieyin <ltieyin@gmail.com>
2021-09-02 10:41:09 +0800

    Create meipian.js

---
+++ Script/meipian.js
@@ -0,0 +1,14 @@
+/*
+美篇会员
+***************************
+QuantumultX:
+[rewrite_local]
+^https:\//poster.meipian.cn\/5.3\/user\/info url script-response-body https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/meipian.js
+[mitm]
+hostname = poster.meipian.cn
+**************************/
+var body = $response.body
+    .replace(/\"member_type\":0/, "\"member_type\":1")
+    .replace(/\"subscribe_product_id\":0/, "\"subscribe_product_id\":2")
+    .replace(/\"next_renew_date\":\"\"/, "\"next_renew_date\":\"2049-12-31\"");
+$done({ body });
