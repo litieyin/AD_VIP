e2f3239d227dfc3488b3fbaadabe720f430b1969
litieyin <ltieyin@gmail.com>
2021-09-02 11:29:08 +0800

    Update people.js

--- Script/zhihu/people.js
+++ Script/zhihu/people.js
@@ -2,4 +2,7 @@ let body = $response.body
 body=JSON.parse(body)
 delete body['mcn_user_info']
 body=JSON.stringify(body)
+    .replace(/\"is_vip\":false/, "\"is_vip\":true")
+    .replace(/\"expires_day\":\"2020-11-08\"/, "\"expires_day\":\"2049-12-31\"");
+
 $done({body})
