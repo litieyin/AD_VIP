19b7be8e65223aa49531d05a7a64b69191bf1cbf
litieyin <ltieyin@gmail.com>
2021-09-02 10:50:56 +0800

    Update meipian.js

--- Script/meipian.js
+++ Script/meipian.js
@@ -10,6 +10,7 @@ hostname = poster.meipian.cn
 var body = $response.body
     .replace(/\"member_type\":0/, "\"member_type\":1")
     .replace(/\"status\":0/, "\"status\":1")
+    .replace(/\"due_days\":0/, "\"due_days\":1000")
     .replace(/\"subscribe_product_id\":0/, "\"subscribe_product_id\":2")
     .replace(/\"next_renew_date\":\"\"/, "\"next_renew_date\":\"2049-12-31\"");
 $done({ body });
