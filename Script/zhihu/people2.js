1a069ef3933144974d52c54580a681a695177895
litieyin <ltieyin@gmail.com>
2021-09-02 12:53:44 +0800

    Update people2.js

--- Script/zhihu/people2.js
+++ Script/zhihu/people2.js
@@ -11,10 +11,6 @@ Quantumult X:
 hostname = ap*.intsig.net
 
 **************************/
-let obj = JSON.parse($response.body);
-obj = {"data":{"menber_infor":{,}}}};
-$done({body: JSON.stringify(obj)});
-let body = $response.body 
 body=JSON.parse(body)
 body.data.menber_infor.vip_type = "svip"
 body.data.menber_infor.vip_tip={"jump_url":"","text":"2049-12-31 到期"}
