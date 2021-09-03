932c62fd41c7bf61f6ebedae7c98f4c061803ff2
litieyin <ltieyin@gmail.com>
2021-09-03 14:19:03 +0800

    Update and rename meipian.js to maimai.js

---
+++ Script/maimai.js
@@ -0,0 +1,13 @@
+/*
+脉脉vip
+***************************
+QuantumultX:
+[rewrite_local]
+^https:\//open.taou.com\/maimai\/member\/uh\/get_member_info_ios_v3 url script-response-body https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/maimai.js
+[mitm]
+hostname = open.taou.com
+**************************/
+var body = $response.body
+    .replace(/\"mem_id.*?mem_bg_pic_url\"/, "\"mem_id\":3,\"mem_name\":VIP\u4f1a\u5458\",\"mem_bg_pic_url\"")
+    .replace(/\"real_expire_time.*?activity\"/, "\"real_expire_time\": \"9999\u5929\u540e\u5230\u671f\", \"real_expire_days\": 9999, \"expire_time_list\": [{\"name\": \"\u804c\u573aVIP\u4f1a\u5458\", \"expire_time\": \"2049-12-31\u5230\u671f\"}]}, \"activactivity\"");
+$done({ body });
