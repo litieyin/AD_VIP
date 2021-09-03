39168dceb02fc36efdfdf5ead30e7c76eaec05dc
litieyin <ltieyin@gmail.com>
2021-09-03 14:53:59 +0800

    Update maimai.js

--- Script/maimai.js
+++ Script/maimai.js
@@ -8,6 +8,5 @@ QuantumultX:
 hostname = open.taou.com
 **************************/
 var body = $response.body
-    .replace(/\"mem_id.*?mem_bg_pic_url\"/, "\"mem_id\":3,\"mem_name\":VIP\\u4f1a\\u5458\",\"mem_bg_pic_url\"")
     .replace(/\"real_expire_days.*?expire_time_list\"/, "\"real_expire_days\": 999, \"expire_time_list\"");
 $done({ body });
