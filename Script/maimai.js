1b12d1f996532e47a51edb66b90fbd87efd13032
litieyin <ltieyin@gmail.com>
2021-09-03 14:53:06 +0800

    Update maimai.js

--- Script/maimai.js
+++ Script/maimai.js
@@ -9,5 +9,5 @@ hostname = open.taou.com
 **************************/
 var body = $response.body
     .replace(/\"mem_id.*?mem_bg_pic_url\"/, "\"mem_id\":3,\"mem_name\":VIP\\u4f1a\\u5458\",\"mem_bg_pic_url\"")
-    .replace(/\"real_expire_time.*?activity\"/, "\"real_expire_time\": \"999\\u5929\\u540e\\u5230\\u671f\", \"real_expire_days\": 999, \"expire_time_list\": [{\"name\": \"\\u804c\\u573aVIP\\u4f1a\\u5458\", \"expire_time\": \"2029-12-31\\u5230\\u671f\"}]}, \"activity\"");
+    .replace(/\"real_expire_days.*?expire_time_list\"/, "\"real_expire_days\": 999, \"expire_time_list\"");
 $done({ body });
