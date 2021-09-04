cce605e4b6a2ddc7aab2a1f157dc2135aa157000
litieyin <ltieyin@gmail.com>
2021-09-04 14:32:56 +0800

    Create mypage.js

---
+++ Script/maimai/mypage.js
@@ -0,0 +1,12 @@
+/*
+脉脉vip
+***************************
+QuantumultX:
+[rewrite_local]
+^https:\//maimai.cn\/bizmember\/vip_center\/me_tab_page_banner url script-response-body https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/maimai/mypage.js
+[mitm]
+hostname = maimai.cn
+**************************/
+var body = $response.body
+    .replace(/\"bgtxt\".*?\"bgimgurl\"/, "\"bgtxt\":\"会员有效期至2029/12/31\",\"bgimgurl\"");
+$done({ body });
