7f5b44533473547f4ad8a36b0588e354fbaee045
litieyin <ltieyin@gmail.com>
2021-09-04 09:30:24 +0800

    Update baobaoshu.js

--- Script/baobaoshu.js
+++ Script/baobaoshu.js
@@ -10,5 +10,6 @@ hostname = m.meitun.com
 var body = $response.body
     .replace(/\"memberState\":0/, "\"memberState\":1")
     .replace(/\"expirationDateDesc\":\"开通宝宝树会员，尊享专属特权\"/, "\"expirationDateDesc\":\"宝宝树会员\"")
+    .replace(/\"vipSwitch\":1/, "\"vipSwitch\":0")
     .replace(/\"expirationDateStr\":\"\"/, "\"expirationDateStr\":\"2049-12-31\"");
 $done({ body });
