d7a1247e6c45f884a1744893762ded7bcf48c4e5
litieyin <ltieyin@gmail.com>
2021-09-04 09:44:01 +0800

    Update baobaoshu.js

--- Script/baobaoshu.js
+++ Script/baobaoshu.js
@@ -10,5 +10,5 @@ hostname = go.babytree.com
 var body = $response.body
     .replace(/\"member_status\":\"0\"/p, "\"member_status\":\"1\"")
     .replace(/\"text\":\"开通宝宝树会员，尊享专属特权\"/p, "\"text\":\"宝宝树会员\"")
-    .replace(/\"expirationDate\":\"\"/p, "\"expirationDate\":\"2049-12-31 23:59:59\"");
+    .replace(/\"expirationDate\":\"\"/p, "\"expirationDate\":\"2524521600\"");
 $done({ body });
