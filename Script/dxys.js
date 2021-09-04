ad9ed437df5da2a380060fdba1f2a30c25443324
litieyin <ltieyin@gmail.com>
2021-09-04 09:07:24 +0800

    Update dxys.js

--- Script/dxys.js
+++ Script/dxys.js
@@ -3,13 +3,13 @@
 ***************************
 QuantumultX:
 [rewrite_local]
-^https:\//dxy.com\/app\/i\/user\/ask\/account url script-response-body https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/dxys.js
+^https:\//dxy.com\/app\/i\/user\/ask\/account\?ac url script-response-body https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/dxys.js
 
 [mitm]
 hostname = dxy.com
 **************************/
 var body = $response.body
-    .replace(/\"type\":0/, "\"premium\":1")
-    .replace(/\"status\":9/, "\"status\":1")
+    .replace(/\"type\":0/, "\"type\":1")
+    .replace(/\"status\":9/, "\"status\":9")
     .replace(/\"expire_timestamp\":0/, "\"expire_timestamp\":2524521600");
 $done({ body });
