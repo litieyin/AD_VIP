c2c7b202d8d31c31003f893b4cb18c0773276af7
litieyin <ltieyin@gmail.com>
2021-09-04 09:40:32 +0800

    Update baobaoshu.js

--- Script/baobaoshu.js
+++ Script/baobaoshu.js
@@ -3,13 +3,12 @@
 ***************************
 QuantumultX:
 [rewrite_local]
-^http:\//m.meitun.com\/newapi\/router\/memberInfo\/queryUserMemberInfo url script-response-body https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/baobaoshu.js
+^http:\//go.babytree.com\/go_pregnancy\/api\/user\/get_user_info url script-response-body https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/baobaoshu.js
 [mitm]
-hostname = m.meitun.com
+hostname = go.babytree.com
 **************************/
 var body = $response.body
-    .replace(/\"memberState\":0/, "\"memberState\":1")
-    .replace(/\"expirationDateDesc\":\"开通宝宝树会员，尊享专属特权\"/, "\"expirationDateDesc\":\"宝宝树会员\"")
-    .replace(/\"vipSwitch\":1/, "\"vipSwitch\":0")
-    .replace(/\"expirationDateStr\":\"\"/, "\"expirationDateStr\":\"2049-12-31 23:59:59\"");
+    .replace(/\"member_status\":\"0\"/p, "\"member_status\":\"1\"")
+    .replace(/\"text\":\"开通宝宝树会员，尊享专属特权\"/p, "\"text\":\"宝宝树会员\"")
+    .replace(/\"expirationDate\":\"\"/p, "\"expirationDate\":\"2049-12-31 23:59:59\"");
 $done({ body });
