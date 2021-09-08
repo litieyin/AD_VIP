cb9fe14476288cfebb758c99f686747eed045e62
litieyin <ltieyin@gmail.com>
2021-09-08 08:50:12 +0800

    Update nfzm.js

--- Script/nfzm.js
+++ Script/nfzm.js
@@ -1,8 +1,4 @@
-/*
-^http:\//api.infzm.com\/mobile\/user url script-response-body https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/nfzm.js
-/
-  
-var body = $response.body
-    .replace(/\"member_type\":2,\"expire_time\":null/, "\"member_type\":1,\"expire_time\":2524521600")
+ var body = $response.body
+    .replace(/\"member_type\":2,\"expire_time\":null/, "\"member_type\":2,\"expire_time\":2524521600")
     .replace(/\"member_type_detail\":\".*?\"/, "\"member_type_detail\":\"\\u5e74\\u4f1a\\u5458\"");
 $done({ body });
