4c9fa5f0b2ac082c3e4e7187e0a33f28b952a96b
litieyin <ltieyin@gmail.com>
2021-09-08 09:01:10 +0800

    Update nfzm.js

--- Script/nfzm.js
+++ Script/nfzm.js
@@ -1,4 +1,4 @@
  var body = $response.body
-    .replace(/\"member_type\":2,\"expire_time\":null/, "\"member_type\":2,\"expire_time\":2524521600")
+    .replace(/\"member_type\":2,\"expire_time\":null/, "\"member_type\":1,\"expire_time\":2524521600")
     .replace(/\"member_type_detail\":\".*?\"/, "\"member_type_detail\":\"\\u5e74\\u4f1a\\u5458\"");
 $done({ body });
