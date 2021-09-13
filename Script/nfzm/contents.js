8e24bb8da52a5eb6cfc036907144d16a324dd2bf
litieyin <ltieyin@gmail.com>
2021-09-13 10:31:40 +0800

    Update contents.js

--- Script/nfzm/contents.js
+++ Script/nfzm/contents.js
@@ -3,5 +3,6 @@
     .replace(/\"mode\":\"meterage\"/g, "\"mode\":\"free\"")
     .replace(/\"is_original\":1/g, "\"is_original\":0")
     .replace(/\"people_magazine_content_id\":.*?,/g, "\"people_magazine_content_id\":0,")
+    .replace(/\"magazine_content_id\":.*?,/g, "\"magazine_content_id\":0,")
     .replace(/\"poster_to_share\":1/g, "\"poster_to_share\":0");
 $done({ body });
