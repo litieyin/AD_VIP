931de19cc890b44d905a307f07570c9b4bbad836
litieyin <ltieyin@gmail.com>
2021-09-14 14:51:33 +0800

    Update sass.js

--- Script/qcc/sass.js
+++ Script/qcc/sass.js
@@ -1,4 +1,4 @@
  var body = $response.body
-    .replace(/\"userType\":1/, "\"userType\":5")
+    .replace(/\"userType\":1/, "\"userType\":2")
     .replace(/\"type\":1/, "\"type\":1");
 $done({ body });
