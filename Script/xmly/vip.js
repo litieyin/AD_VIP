04329b662ba8a02255fbf8877e1a01a845c4263a
litieyin <ltieyin@gmail.com>
2021-09-15 09:23:19 +0800

    Update vip.js

--- Script/xmly/vip.js
+++ Script/xmly/vip.js
@@ -1,3 +1,3 @@
 var body = $response.body
-    .replace(/\"vipStatus\":3.*?autoRenew/, "\"vipStatus\":5,\"vipDescription\":\"VIP会员\",\"expireDate\":\"2099-12-31\",\"expireDays\":0,\"autoRenew");
+    .replace(/\"vipStatus\":3.*?autoRenew/, "\"vipStatus\":2,\"vipDescription\":\"VIP会员\",\"expireDate\":\"2099-12-31\",\"expireDays\":0,\"autoRenew");
 $done({ body });
