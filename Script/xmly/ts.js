5a61d6e64b6440c60befe0c49217249a8ecf6903
litieyin <ltieyin@gmail.com>
2021-09-15 13:51:31 +0800

    Update ts.js

--- Script/xmly/ts.js
+++ Script/xmly/ts.js
@@ -1,4 +1,4 @@
 var body = $response.body
-    .replace(/\"vipStatus\":3/, "\"vipStatus\":4")
+    .replace(/\"vipStatus\":3/, "\"vipStatus\":1")
     .replace(/\"expireTime\":1547308799000/, "\"expireTime\":2524521600000");
 $done({ body });
