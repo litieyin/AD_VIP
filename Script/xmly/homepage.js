1ce68d278a72a2703ccf31def9c203340e1dbf2d
litieyin <ltieyin@gmail.com>
2021-09-15 12:14:46 +0800

    Update homepage.js

--- Script/xmly/homepage.js
+++ Script/xmly/homepage.js
@@ -1,5 +1,5 @@
 var body = $response.body
-    .replace(/\"isVip\":false.*?vipUrl/, "\"isVip\":true,\"vipExpireTime\":2524521600000,\"vipStatus\":6,\"vipTip\":\"尊贵的VIP会员\",\"vipUrl")
+    .replace(/\"isVip\":false.*?vipUrl/, "\"isVip\":true,\"vipExpireTime\":2524521600000,\"vipStatus\":4,\"vipTip\":\"尊贵的VIP会员\",\"vipUrl")
     .replace(/\"isVip\":false/g, "\"isVip\":true")
     .replace(/\"name\":\".*?\"/, "\"name\":\"大圣人\"")
     .replace(/\"userTitle\":\".*?\"/, "\"userTitle\":\"大圣人\"");
