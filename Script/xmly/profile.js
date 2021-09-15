d37bf9e372fe4f9142d7a19dc6a460888b5a4e66
litieyin <ltieyin@gmail.com>
2021-09-15 11:39:29 +0800

    Update profile.js

--- Script/xmly/profile.js
+++ Script/xmly/profile.js
@@ -1,3 +1,4 @@
 var body = $response.body
-    .replace(/\"value\":0,\"level\":0,\"expire\":null,\"state\":2,\"today\":0/, "\"value\":4,\"level\":5,\"expire\":2524521600000,\"state\":2,\"today\":5");
+    .replace(/\"value\":0,\"level\":0,\"expire\":null,\"state\":2,\"today\":0/, "\"value\":25000,\"level\":5,\"expire\":2524521600000,\"state\":2,\"today\":5")
+    .replace(/\"ret\":0/, "\"ret\":99999");
 $done({ body });
