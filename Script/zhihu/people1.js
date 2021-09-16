50b272d12c51a12c97f37fcbf05b7423e760dba8
litieyin <ltieyin@gmail.com>
2021-09-16 16:55:47 +0800

    Update people1.js

--- Script/zhihu/people1.js
+++ Script/zhihu/people1.js
@@ -1,5 +1,5 @@
 var body = $response.body
-    .replace(/\"expires_at\":.*?,/g, "\"expires_at\":2524521600,")
+    .replace(/\"expires_at\":.*?,/g, "\"expires_at\":4102415999,")
     .replace(/\"is_active\":false/g, "\"is_active\":true")
     .replace(/\"is_expired\":true/g, "\"is_expired\":false")
     .replace(/\"alert\":.*?\,/g, "\"alert\":\"永久会员\",");
