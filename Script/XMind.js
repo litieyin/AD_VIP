c1228733d4a00edd35ec44400b1b14b70edb8d88
litieyin <ltieyin@gmail.com>
2021-09-16 17:01:26 +0800

    Update XMind.js

--- Script/XMind.js
+++ Script/XMind.js
@@ -4,7 +4,7 @@ obj = {
    "raw_data": "DBcBHgojrvPgruIJMfb4KK/76CmjxSHSp9KipRwOnQUuf+Gt2FncFUzNvxZydUeXEzDZt/XWEm91lHFYrvT0f2AFap2L4psLI/34sKU9VLGH7kFsBYlexU/nifBtosMJqQxL4TU1pjvjl+1uyWsFAeGR42aEnVhQWgvJB5Ovcd0=",
    "license": {
      "status": "sub",
-     "expireTime": 3070928235000
+     "expireTime": 4102415999000
    },
    "_code": 200
  }
