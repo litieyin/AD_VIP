1024ab6fd4b7e666144f743447c0617ec21bf99c
litieyin <ltieyin@gmail.com>
2021-09-16 16:54:08 +0800

    Update ability.js

--- Script/migu/ability.js
+++ Script/migu/ability.js
@@ -8,9 +8,9 @@ QuantumultX:
 hostname = v.miguvideo.com
 **************************/
 var body = $response.body
-    .replace(/\"expireTime\":\"2021-09-29 16:50:52\"/, "\"expireTime\":\"2049-12-31 23:59:59\"")
-    .replace(/\"nextPaidTime\":\"2021-09-29 16:50:53\"/, "\"nextPaidTime\":\"2049-12-31 23:59:59\"")
-    .replace(/\"endTime\":\"20210929165037\"/, "\"endTime\":\"20491231235959\"")
+    .replace(/\"expireTime\":\"2021-09-29 16:50:52\"/, "\"expireTime\":\"2099-12-31 23:59:59\"")
+    .replace(/\"nextPaidTime\":\"2021-09-29 16:50:53\"/, "\"nextPaidTime\":\"2099-12-31 23:59:59\"")
+    .replace(/\"endTime\":\"20210929165037\"/, "\"endTime\":\"20991231235959\"")
     .replace(/\"expiredCardImg\":\"\"/, "\"timesFlag\":\"0\"")
     .replace(/\"expiredDay\":\"0\"/, "\"sealedFlag\":\"0\"")
     .replace(/\"effectiveFlag\":\"0\"/, "\"effectiveFlag\":\"1\"");
