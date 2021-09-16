0fa2ede068601a6f0a36c7bcacb1dedf316eb1b4
litieyin <ltieyin@gmail.com>
2021-09-16 16:54:23 +0800

    Update memberinfo.js

--- Script/migu/memberinfo.js
+++ Script/migu/memberinfo.js
@@ -9,7 +9,7 @@ QuantumultX:
 hostname = v.miguvideo.com
 **************************/
 var body = $response.body
-    .replace(/\"2021-09-29 16:50:52\"/p, "\"2049-12-31 23:59:59\"")
+    .replace(/\"2021-09-29 16:50:52\"/p, "\"2099-12-31 23:59:59\"")
     .replace(/\"isDiamond\":[true|false]/, "\"isDiamond\":true")
     .replace(/\"skipBeforeAd\":[true|false]/, "\"skipBeforeAd\":true")
     .replace(/\"isVip\":[true|false]/, "\"isVip\":true");
