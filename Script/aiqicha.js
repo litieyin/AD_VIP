872044cf3d8a28d9ebc296c53db563eca8bd3c4a
litieyin <ltieyin@gmail.com>
2021-09-16 17:01:02 +0800

    Update aiqicha.js

--- Script/aiqicha.js
+++ Script/aiqicha.js
@@ -8,7 +8,7 @@ let obj = JSON.parse($response.body);
     obj.data = {
     "vip": 1,
     "consume": 150,
-    "time": "2049-12-31",
+    "time": "2099-12-31",
     "signInStaus": 0
   }
 $done({body: JSON.stringify(obj)});
