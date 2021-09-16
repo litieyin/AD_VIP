3bd2cd4aaa83fc630e25456a993b5ce4ffa2d565
litieyin <ltieyin@gmail.com>
2021-09-16 14:47:59 +0800

    Update article.js

--- Script/jiemian/article.js
+++ Script/jiemian/article.js
@@ -1,4 +1,6 @@
 var body = $response.body
     .replace(/\"pay_status_info\":.*?\}/, "\"pay_status_info\" : null")
+    .replace(/\"comment_count\":.*?,/, "\"comment_count\":\"0\",")
+    .replace(/\"ding_count\":.*?,/, "\"ding_count\":\"0\",")
     .replace(/\"is_pay\":1/g, "\"is_pay\":0");
 $done({ body });
