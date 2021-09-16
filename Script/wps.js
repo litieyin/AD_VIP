0fa6f23184428b98281aa1c76e603fd748b0939a
litieyin <ltieyin@gmail.com>
2021-09-16 16:57:33 +0800

    Update wps.js

--- Script/wps.js
+++ Script/wps.js
@@ -17,12 +17,12 @@ var obj = {
   exp: 0,
   level: 3,
   privilege: [
-    { spid: "data_recover", times: 0, expire_time: 2524579199 },
-    { spid: "ocr", times: 0, expire_time: 2524579199 },
-    { spid: "pdf2doc", times: 0, expire_time: 2524579199 },
-    { spid: "pdf_merge", times: 0, expire_time: 2524579199 },
-    { spid: "pdf_sign", times: 0, expire_time: 2524579199 },
-    { spid: "pdf_split", times: 0, expire_time: 2524579199 }
+    { spid: "data_recover", times: 0, expire_time: 4102415999 },
+    { spid: "ocr", times: 0, expire_time: 4102415999 },
+    { spid: "pdf2doc", times: 0, expire_time: 4102415999 },
+    { spid: "pdf_merge", times: 0, expire_time: 4102415999 },
+    { spid: "pdf_sign", times: 0, expire_time: 4102415999 },
+    { spid: "pdf_split", times: 0, expire_time: 4102415999 }
   ],
   result: "ok",
   total_buy: 0,
@@ -32,15 +32,15 @@ var obj = {
     name: "超级会员",
     has_ad: 0,
     memberid: 40,
-    expire_time: 2524579199,
+    expire_time: 4102415999,
     enabled: [
-      { memberid: 40, name: "超级会员", expire_time: 2524579199 },
-      { memberid: 20, name: "WPS会员", expire_time: 2524579199 },
-      { memberid: 12, name: "稻壳会员", expire_time: 2524579199 }
+      { memberid: 40, name: "超级会员", expire_time: 4102415999 },
+      { memberid: 20, name: "WPS会员", expire_time: 4102415999 },
+      { memberid: 12, name: "稻壳会员", expire_time: 4102415999 }
     ]
   },
   wealth: 0,
-  expire_time: 2524579199
+  expire_time: 4102415999
 };
 
 $done({ body: JSON.stringify(obj) });
