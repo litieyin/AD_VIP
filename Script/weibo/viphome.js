73420fb8c380302cf71f46f32cae8ebecc892640
litieyin <ltieyin@gmail.com>
2021-10-13 14:40:04 +0800

    Update viphome.js

--- Script/weibo/viphome.js
+++ Script/weibo/viphome.js
@@ -1,8 +1,8 @@
 var body = $response.body
-    .replace(/\"level\":2,\"vip_identity\":3/, "\"level\":7,\"vip_identity\":2")
+    .replace(/\"level\":2/, "\"level\":7")
     .replace(/\"s_type\":0/, "\"s_type\":1")
     .replace(/\"type\":2/, "\"type\":1")
     .replace(/\"s_type\":0/, "\"s_type\":1")
     .replace(/\"desc\":\".*?\"/,"\"desc\":\"\\u6c38\\u4e45\\u4f1a\\u5458\"")
-    .replace(/\"expired_days\":.*?,/,"\"expired_days\":,");
+    .replace(/\"expired_days\":.*?,/,"\"expired_days\":0,");
 $done({ body });
