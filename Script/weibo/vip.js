614611be9590af7fa003384a7e5e2ef9cb8a2eb9
litieyin <ltieyin@gmail.com>
2021-10-13 14:15:11 +0800

    Update vip.js

--- Script/weibo/vip.js
+++ Script/weibo/vip.js
@@ -2,5 +2,6 @@ var body = $response.body
     .replace(/\"level\":2,\"vip_identity\":3/, "\"level\":7,\"vip_identity\":5")
     .replace(/\"is_baoyue\":0/, "\"is_baoyue\":1")
     .replace(/\"baoyue_type\":false/, "\"baoyue_type\":true")
+    .replace(/\"type\":0/, "\"type\":1")
     .replace(/\"desc\":\".*?\"/,"\"desc\":\"\\u6c38\\u4e45\\u4f1a\\u5458\"");
 $done({ body });
