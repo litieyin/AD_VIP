5526a4267b94d2fb4f93fba2b6beff6daa1955ab
litieyin <ltieyin@gmail.com>
2021-10-20 17:08:53 +0800

    Update vip1.js

--- Script/tianya/vip1.js
+++ Script/tianya/vip1.js
@@ -1,5 +1,3 @@
  var body = $response.body
-    .replace(/天涯贝：<span><\/span><\/p >
-            <p class=\"deadline\">会员有效期至 :/, "天涯贝：999<span><\/span><\/p >
-            <p class=\"deadline\">会员有效期至 :2099-12-31 ");
+    .replace(/: <span/, ":2099-12-31 <span");
 $done({ body });
