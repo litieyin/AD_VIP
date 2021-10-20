e84b4f323cb869ccd0e02efcc982ba19c812b5cb
litieyin <ltieyin@gmail.com>
2021-10-20 16:37:43 +0800

    Update vip.js

--- Script/tianya/vip.js
+++ Script/tianya/vip.js
@@ -1,8 +1,3 @@
  var body = $response.body
-    .replace(/\"isview\":false/g, "\"isview\":true")
-    .replace(/\"mode\":\"meterage\"/g, "\"mode\":\"free\"")
-    .replace(/\"is_original\":1/g, "\"is_original\":0")
-    .replace(/\"people_magazine_content_id\":.*?,/g, "\"people_magazine_content_id\":0,")
-    .replace(/\"magazine_content_id\":.*?,/g, "\"magazine_content_id\":0,")
-    .replace(/\"poster_to_share\":1/g, "\"poster_to_share\":0");
+    .replace(/\"isVipUser\":false/, "\"isVipUser\":true");
 $done({ body });
