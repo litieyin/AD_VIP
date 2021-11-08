4735472d72261882dc6f01f0405a6e399752e07d
litieyin <ltieyin@gmail.com>
2021-11-08 08:35:32 +0800

    Create meipian.js

---
+++ Script/meipian.js
@@ -0,0 +1,6 @@
+**************************/
+var body = $response.body
+    .replace(/\"member_type\":0/, "\"member_type\":2")
+    .replace(/\"status\":0/, "\"status\":1")
+    .replace(/\"expired_at_str\":\"\"/, "\"expired_at_str\":\"2099-12-31\"");
+$done({ body });
