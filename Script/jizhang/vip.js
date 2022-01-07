9142b2d881a9ef73c7b30fb845b42cc4f8d0545d
litieyin <ltieyin@gmail.com>
2022-01-07 14:58:54 +0800

    Update vip.js

--- Script/jizhang/vip.js
+++ Script/jizhang/vip.js
@@ -1,4 +1,4 @@
 var body = $response.body
-    .replace(/\"expire_date\":0/, "\"expire_date\":\"2099-12-31\"")
+    .replace(/\"expire_date\":0/, "\"expire_date\":4102383492")
     .replace(/\"vip_type\":\[\]/, "\"vip_type\":\"mymoney\.YearlyMembership\"");
 $done({ body });
