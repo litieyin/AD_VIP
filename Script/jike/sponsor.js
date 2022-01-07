a2c5f5fad4d4ae28e3292fd737fb06159994bb7e
litieyin <ltieyin@gmail.com>
2022-01-07 09:17:41 +0800

    Update sponsor.js

--- Script/jike/sponsor.js
+++ Script/jike/sponsor.js
@@ -1,11 +1,4 @@
 **************************/
 var body = $response.body
-    .replace(/\"isActive\":false/g, "\"isActive\":true")
-    .replace(/\"paymentType\":\"\"/, "\"paymentType\":\"CHARGE\"")
-    .replace(/\"status\":\"\"/, "\"status\":\"续费Jike Yellow\"")
-    .replace(/\"productId\":\"\"/, "\"productId\":\"com.ruguoapp.jike.once3year\"")
-    .replace(/\"subscribedProductId\":\"\"/, "\"subscribedProductId\":\"com.ruguoapp.jike.once3year\"")
-    .replace(/\"platform\":\"\"/, "\"platform\":\"IOS\"")
-    .replace(/\"sponsorId\":0/, "\"sponsorId\":2")
-    .replace(/\"days\":0/, "\"days\":36");
+    .replace(/\"isActive\":false,\"paymentType\":\"\",\"status\":\"\",\"productId\":\"\",\"subscribedProductId\":\"\",\"platform\":\"\",\"sponsorId\":0,\"days\":0/, "\"isActive\":true,\"paymentType\":\"CHARGE\",\"status\":\"续费Jike Yellow\",\"productId\":\"com\.ruguoapp\.jike\.once3year\",\"subscribedProductId\":\"com\.ruguoapp\.jike\.once3year\",\"platform\":\"IOS\",\"sponsorId\":1,\"days\":36");
 $done({ body });
