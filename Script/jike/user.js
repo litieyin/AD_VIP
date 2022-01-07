a8327132713df37278ae9efdeccf43ce1946fb2f
litieyin <ltieyin@gmail.com>
2022-01-07 09:14:18 +0800

    Update user.js

--- Script/jike/user.js
+++ Script/jike/user.js
@@ -1,5 +1,5 @@
 **************************/
 var body = $response.body
-    .replace(/\"isSponsor\":false/, "\"isSponsor\":true")
+    .replace(/\"isSponsor\":false/g, "\"isSponsor\":true")
     .replace(/\"valid\":false/, "\"valid\":true");
 $done({ body });
