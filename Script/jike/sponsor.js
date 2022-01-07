04d2c26e713897d29fc6a3164c760ec69211ac0c
litieyin <ltieyin@gmail.com>
2022-01-07 09:23:14 +0800

    Delete sponsor.js

--- Script/jike/sponsor.js
+++
@@ -1,4 +0,0 @@
-**************************/
-var body = $response.body
-    .replace(/\"isActive\":false,\"paymentType\":\"\",\"status\":\"\",\"productId\":\"\",\"subscribedProductId\":\"\",\"platform\":\"\",\"sponsorId\":0,\"days\":0/, "\"isActive\":true,\"paymentType\":\"CHARGE\",\"status\":\"续费Jike Yellow\",\"productId\":\"com\.ruguoapp\.jike\.once3year\",\"subscribedProductId\":\"com\.ruguoapp\.jike\.once3year\",\"platform\":\"IOS\",\"sponsorId\":1,\"days\":36");
-$done({ body });
