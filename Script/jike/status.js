ee289d716a483bac08f645c4135026d0274c6765
litieyin <ltieyin@gmail.com>
2022-05-07 08:57:33 +0800

    Update status.js

--- Script/jike/status.js
+++ Script/jike/status.js
@@ -1,5 +1,5 @@
 **************************/
 var body = $response.body
-    .replace(/\"isActive\":false,\"paymentType\":\"\",\"status\":\"\",\"productId\":\"\",\"subscribedProductId\":\"\",\"platform\":\"\",\"sponsorId\":0,\"days\":0/, "\"isActive\":true,\"paymentType\":\"CHARGE\",\"status\":\"\",\"productId\":\"com.ruguoapp.jike.once3year\",\"subscribedProductId\":\"\",\"platform\":\"\",\"sponsorId\":0,\"days\":999");
+    .replace(/\"isActive\":false,\"paymentType\":\"\",\"status\":\"\",\"productId\":\"\",\"subscribedProductId\":\"\",\"platform\":\"\",\"sponsorId\":0,\"days\":0/, "\"isActive\":true,\"paymentType\":\"CHARGE\",\"status\":\"\",\"productId\":\"com.ruguoapp.jike.once3year\",\"subscribedProductId\":\"\",\"platform\":\"\",\"sponsorId\":0,\"days\":0");
 
 $done({ body });
