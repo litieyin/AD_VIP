740e05df370825c75ac9e20cfc928b1c41cf6c35
litieyin <ltieyin@gmail.com>
2022-05-07 08:40:17 +0800

    Update jike.js

--- Script/jike.js
+++ Script/jike.js
@@ -1,4 +1,5 @@
 **************************/
 var body = $response.body
-    .replace(/\"isSponsor\":false/, "\"isSponsor\":true");
+    .replace(/\"isSponsor\":false/, "\"isSponsor\":true")
+    .replace(/\"2021-01-01T16:00:00.000Z\"/, "\"2099-01-01T16:00:00.000Z\"");
 $done({ body });
