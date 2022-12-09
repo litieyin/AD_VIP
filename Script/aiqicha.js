7a457a4cf422db93fbce8c8e9d402933444f0e62
litieyin <ltieyin@gmail.com>
2022-12-09 17:12:03 +0800

    Update aiqicha.js

--- Script/aiqicha.js
+++ Script/aiqicha.js
@@ -6,9 +6,6 @@ mitm= aiqicha.baidu.com
 
 let obj = JSON.parse($response.body);
     obj.data = {
-    "vip": 1,
-    "consume": 150,
-    "time": "2099-12-31",
-    "signInStaus": 0
+    {"status":0,"msg":"","data":{"vip":1,"time":"2099-12-31","signInStaus":0,"consume":5,"svip":{"status":1,"startTime":"","endTime":""},"yuqing_junior":{"status":0,"startTime":"","endTime":""},"yuqing_senior":{"status":0,"startTime":"","endTime":""},"asset":{"status":0,"startTime":"","endTime":""},"icp":{"status":0,"startTime":"","endTime":""},"isHitOptimize":0,"isDisplayPop":0}}
   }
 $done({body: JSON.stringify(obj)});
