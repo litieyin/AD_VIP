f3c1df114afd69388cba86caea564c55824ffa05
litieyin <ltieyin@gmail.com>
2023-03-27 09:36:20 +0800

    Update hupu.js

--- Script/hupu.js
+++ Script/hupu.js
@@ -1,7 +1,5 @@
-var obj = JSON.parse($response.body);
-
-obj = {
-   {"code":1,"message":"成功","data":{"isVip":1,"cardNum":4,"nickname":"啊喔呃咦唔吁啵嘙嗼哹","header":"https://i3.hoopchina.com.cn/user/545/35652426969545/35652426969545-1526204680.jpeg@194h_194w_2e","expiredTime":4102415999,"cardType":4,"isSub":0,"cardColor":null,"cardNightColor":null,"cardBackColor":null,"cardNightBackColor":null,"cardIcon":null,"cardNightIcon":null,"rights":null,"isNFTHeader":0}}
- }
-
-$done({body: JSON.stringify(obj)});
+var body = $response.body
+    .replace(/\"isVip\":0/, "\"isVip\":1")
+    .replace(/\"cardType\":null/, "\"cardType\":4")
+    .replace(/\"expiredTime\":null/, "\"expiredTime\":4102415999000");
+$done({ body });
