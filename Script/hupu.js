7c3989598995142bda3a9b6d62f97f1290a02be6
litieyin <ltieyin@gmail.com>
2023-03-27 09:59:38 +0800

    Update hupu.js

--- Script/hupu.js
+++ Script/hupu.js
@@ -2,5 +2,5 @@ var body = $response.body
     .replace(/\"isVip\":0/, "\"isVip\":1")
     .replace(/\"cardType\":null,\"isSub\":null/, "\"cardType\":4,\"isSub\":0")
     .replace(/\"cardNum\":null/, "\"cardNum\":999")
-    .replace(/\"expiredTime\":null/, "\"expiredTime\":41023296");
+    .replace(/\"expiredTime\":null/, "\"expiredTime\":4102329611");
 $done({ body });
